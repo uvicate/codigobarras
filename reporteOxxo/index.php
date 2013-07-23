b8522269b6262ab73bd8997daef9a120979cfb00
elguapo <trasher_17@hotmail.com>
2013-07-23 09:26:00 -0600

    Se realizaron cambios en el index.php
    se saco el post de la etiqueta php n.n

--- reporteOxxo/index.php
+++ reporteOxxo/index.php
@@ -1,10 +1,8 @@
 <?php
 
-	<form method="post" action="subir_archivo.php" enctype="multipart/form-data" id="testform">
+?>	
+<form method="post" action="subir_archivo.php" enctype="multipart/form-data" id="testform">
 	Archivo
-		<input name="archivo" type="file" id="archivo">
-		<input name="boton" type="submit" id="boton" value="Subir">
-	</form>
-
-
-?>
+	<input name="archivo" type="file" id="archivo">
+	<input name="boton" type="submit" id="boton" value="Subir">
+</form>
