46a956e5f08b0a40275e5d4266dac8882954795f
alex0a2 <aalmodovard@gmail.com>
2013-07-26 10:52:09 -0600

    cambio de prueba alexisSinropa.js
    Solo es un cambio para detectar posibles cambios

--- js/alexisSinRopa.js
+++ js/alexisSinRopa.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
+$(document).ready(function() {
+	
 
-
+console.log ("cambio de prueba 26 jul 2013");
 
 
 $(document).ready(function() {
 	
 
-console.log ("tu vagina esta peluda");
-
-=======
-$(document).ready(function() {
-	
->>>>>>> 0f9f1f624bc94bbb4a95fbd71d450931677e1848
 });
