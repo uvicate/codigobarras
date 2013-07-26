01ebb656e1b68ee997daf0fb64bd6ad95bd7bb0c
elguapo <trasher_17@hotmail.com>
2013-07-26 11:43:30 -0600

    SubCarpeteo
    se modificó la jerarquia del proyecto para agregar un index general

---
+++ codigobarras/js/alexisSinRopa.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+
+prueba 2000
+
+
+$(document).ready(function() {
+	
+
+console.log ("tu vagina esta peluda");
+
+=======
+$(document).ready(function() {
+	
+>>>>>>> 0f9f1f624bc94bbb4a95fbd71d450931677e1848
+});
