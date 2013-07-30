a51223d7a105ad45c9e3396d7e2837a6d32a3bf8
alex0a2 <aalmodovard@gmail.com>
2013-07-30 11:47:44 -0600

    archivo utilizado para realizar consultas con la base de datos

--- reporteOxxo/php/subir_archivo.php
+++ reporteOxxo/php/subir_archivo.php
@@ -1,63 +1,66 @@
 
-<table align="center">
- <tr>
-  <td>
-   <b>Nombre:</b>: <?php echo $_FILES["archivo"]["name"]?>
+  <table align="center">
+   <tr>
+    <td>
+     <b>Nombre:</b>: <?php echo $_FILES["archivo"]["name"]?>
+
+     <b>Tipo:</b>: <?php echo $_FILES["archivo"]["type"]?>
+
+     <b>Subida:</b>: <?php echo ($_FILES["archivo"]["error"]) ? "Incorrecta" : "Correcta"?>
+
+     <b>TamaÃ±o:</b>: <?php echo $_FILES["archivo"]["size"]?> bytes
+   </td>
+  </tr>
+  </table>
+
+
+  <?php
+
+  class Datos {
+
+    function leerArchivo (){
+
+         //SI EL ARCHIVO SE ENVIA Y ADEMAS SE SUBIO CORRECTAMENTE
+      if (isset($_FILES["archivo"]) && is_uploaded_file($_FILES['archivo']['tmp_name'])) {
+
+        //SE ABRE EL ARCHIVO EN MODO LECTURA
+       $fp = fopen($_FILES['archivo']['tmp_name'], "r");
+
+        //RECORRER
+       while (!feof($fp)){ //LEE EL ARCHIVO A DATA, LO VECTORIZA A DATA
+
+        //preg_match_all('[a-z]');
+
+        //SI SE LEE SEPARADO POR COMAS
+        $data  = explode(",", fgets($fp));
+        
+        //-> COMENTARIOS ALEXIS 
+        //->CICLO DE LECTURA DE $DATA
+          foreach ($data as $key => $value) {
+
+                echo($key.' '.$value);
+                echo "<br/>";
+
+               }
        
-   <b>Tipo:</b>: <?php echo $_FILES["archivo"]["type"]?>
-       
-   <b>Subida:</b>: <?php echo ($_FILES["archivo"]["error"]) ? "Incorrecta" : "Correcta"?>
-       
-   <b>TamaÃ±o:</b>: <?php echo $_FILES["archivo"]["size"]?> bytes
-  </td>
- </tr>
-</table>
+
+        //NOTA CADA VUELTA EQUIVALE A UNA LINEA COMPLETA DEL ARCHIVO CSV
+     } 
+
+     echo "Archivo recorrido alexis";
+
+   } else 
+   echo "Error de subida";
 
 
-<?php
-//SI EL ARCHIVO SE ENVIÃ“ Y ADEMÃS SE SUBIO CORRECTAMENTE
-if (isset($_FILES["archivo"]) && is_uploaded_file($_FILES['archivo']['tmp_name'])) {
- 
- //SE ABRE EL ARCHIVO EN MODO LECTURA
- $fp = fopen($_FILES['archivo']['tmp_name'], "r");
- //SE RECORRE
- while (!feof($fp)){ //LEE EL ARCHIVO A DATA, LO VECTORIZA A DATA
-
-  //preg_match_all('[a-z]');
-  
-  //SI SE QUIERE LEER SEPARADO POR TABULADORES
-   $data  = explode(",", fgets($fp));
-  //SI SE LEE SEPARADO POR COMAS
-  //$data  = explode(",", fgets($fp));
-  
-  //AHORA DATA ES UN VECTOR Y EN CADA POSICIÃ“N CONTIENE UN VALOR QUE ESTA SEPARADO POR COMA.
-  //EJEMPLO    A, B, C, D
-  //$data[0] CONTIENE EL VALOR "A", $data[1] -> B, $data[2] -> C.
 
 
-  //SI QUEREMOS VER TODO EL CONTENIDO EN BRUTO:
-  foreach ($data as $key => $value) {
-
-       echo($key.' '.$value);
-       echo "<br/>";
-    
   }
- 
-  
-  //print_r($data[1]);
-  // echo "<br/>";
+}
+
+$datos = new Datos();
+$datos -> leerArchivo();
 
 
-  //SI QUEREMOS IMPRIMIR UN SOLO DATO
-  //echo "<br/>Imprimir el primer dato solo: {$data[0]}<br/>";
 
-
-  //NOTA CADA VUELTA EQUIVALE A UNA LINEA COMPLETA DEL ARCHIVO CSV
- } 
-   
- echo "Archivo recorrido alexis";
-   
-} else 
- echo "Error de subida";
-
-?>  
+  ?>  
