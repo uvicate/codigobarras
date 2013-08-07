70dc29a1b276484ccca3a6226961cecedca85370
alex0a2 <aalmodovard@gmail.com>
2013-08-07 10:46:52 -0600

    se esta modificando las funciones contenidas
    se estan dividiendo en funciones , la parte donde lee datos del .txt y
    la parte donde se insertan los datos a la base de datos

--- reporteOxxo/php/transaccionesArchivo.class.php
+++ reporteOxxo/php/transaccionesArchivo.class.php
@@ -6,55 +6,59 @@
 
   class Datos {
 
-    function leerArchivo (){
-
+    public function leerArchivo (){
          //SI EL ARCHIVO SE ENVIA Y ADEMAS SE SUBIO CORRECTAMENTE
-      if (isset($_FILES["archivo"]) && is_uploaded_file($_FILES['archivo']['tmp_name'])) {
+          if (isset($_FILES["archivo"]) && is_uploaded_file($_FILES['archivo']['tmp_name'])) {
+              //SE ABRE EL ARCHIVO EN MODO LECTURA
+             $fp = fopen($_FILES['archivo']['tmp_name'], "r");
+              //RECORRER $fp
+             while (!feof($fp)){ //LEE EL ARCHIVO A DATA, LO VECTORIZA A DATA
+              //SI SE LEE SEPARADO POR COMAS
+              $data  = explode(",", fgets($fp)); 
+             } 
+              echo "Archivo recorrido alexis";
 
-        //SE ABRE EL ARCHIVO EN MODO LECTURA
-       $fp = fopen($_FILES['archivo']['tmp_name'], "r");
+          } else{ 
+              echo "Error de subida";
+            }         
+          return $data;
+    } 
 
-        //RECORRER
-       while (!feof($fp)){ //LEE EL ARCHIVO A DATA, LO VECTORIZA A DATA
+    public function guardarDatosTxt() {
 
-        //preg_match_all('[a-z]');
+      $datos = $this-> leerArchivo();
+      // global $conexion;
 
-        //SI SE LEE SEPARADO POR COMAS
-        $data  = explode(",", fgets($fp));
+      //          $idOxxo = $obteniendo -> getIdOxxo();
+      //          $idCliente = $object["idCliente"];
+      //          $idClienteUsuario = $object["idClienteUsuario"];
+      //          $idUsuario = $object["idUsuario"];
+      //          $fecha = $obteniendo -> getFecha();
+      //          $monto = $obteniendo -> getMonto();
 
-            global $conexion;
+      //          preg_match('(^\d{2}$)', $idOxxo, $matchIdOxxo);
+      //          preg_match('(^\d{1,2}$)', $idCliente, $matchIdCliente);
+      //          preg_match('(^\d{1,4}$)', $idClienteUsuario, $matchIdClienteUsuario);
+      //          preg_match('(^\d{1,7}$)', $idUsuario, $matchIdUsuario);
+      //          preg_match('(^\d{8}$)', $fecha, $matchFecha);
+      //          preg_match('(^\d{1,7}$)', $monto, $matchMonto);
 
-            $query_sql = "INSERT INTO recibosOxxo(nombrePlazaOxxo,nombreTiendaOxxo,fechaPagoEnOxxo,horaPagoEnOxxo,barcodeReciboPt1,barcodeReciboPt2,importe) 
-            VALUES('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."','".$data[5]."','".$data[6]."')";
-        
-            $conexion -> consulta($query_sql);
-
-     } 
-
-     echo "Archivo recorrido alexis";
-
-   } else 
-   echo "Error de subida";
-
-
-  }
-
-  public function guardarDatosTxt() {
-    global $conexion;
-
-    $query_sql = "INSERT INTO recibosOxxo(nombrePlazaOxxo,nombreTiendaOxxo,fechaPagoEnOxxo,horaPagoEnOxxo,barcodeReciboPt1,barcodeReciboPt2,importe) 
-    VALUES('Plaza cruz','Faja de Oro BJX','20101227','16:52','370000048197280220110137','926000000000000000000000',0000000004235.00)";
-    
-    $conexion -> consulta($query_sql);
-    $query_php = $conexion -> datos(true);
-  }
+      //          $query_sql = "INSERT INTO recibosOxxo(nombrePlazaOxxo,nombreTiendaOxxo,fechaPagoEnOxxo,horaPagoEnOxxo,barcodeReciboPt1,barcodeReciboPt2,importe) 
+      //          VALUES('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."','".$data[5]."','".$data[6]."')";
+          
+      //          $conexion -> consulta($query_sql);
+    }
     
 
 
 }
 
 $datos = new Datos();
-$datos -> leerArchivo();
+$d= $datos -> leerArchivo();
+print_r($d);
+
+
+
 
 
 
